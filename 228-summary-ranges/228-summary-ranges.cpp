42c697181175d23c8ca4a310ebf8a03773bb0dd4
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-02-28 16:48:25 +0530

    Time: 6 ms (7.02%), Space: 6.9 MB (14.46%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -5,7 +5,7 @@ public:
 	for (auto p(0), q(0); q<size(n); ++q)
 		if (q+1==size(n) || n[q+1]!=n[q]+1)
 		{
-			r.emplace_back( p!=q ? to_string(n[p])+"->"+to_string(n[q]) : to_string(n[p]) );
+			r.push_back( p!=q ? to_string(n[p])+"->"+to_string(n[q]) : to_string(n[p]) );
 			p=q+1;
 		}
 	return r;
