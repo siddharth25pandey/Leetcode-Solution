eeee2f55d552751037d0e1bf61e7ce26a22f1b43
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-03-23 22:06:29 +0530

    Time: 4 ms (21.20%), Space: 5.8 MB (97.08%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int climbStairs(int n) {
-         vector<long>steps;
-steps.push_back(1);
-steps.push_back(2);
-for(int i=2;i<=n;i++)
+        vector<int>steps;
+/*steps.push_back(1);
+steps.push_back(2);*/
+long first=1, second=2;
+long res=0;
+    if(n==1) return 1;
+        if(n==2) return 2;
+for(int i=3;i<=n;i++)
 {
-	steps.push_back(steps[i-1]+steps[i-2]);
+	//steps.push_back(steps[i-1]+steps[i-2]);
+	res=first+second;
+	first= second;
+	second=res;
 }
-return steps[n-1];
+return res;
+//return steps[n-1];
+
 
     }
 };
