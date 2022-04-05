6a2eda16bdd0d4444496c07dc9aa3febb24e92d5
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-04-05 23:46:40 +0530

    Time: 7 ms (81.00%), Space: 10.4 MB (34.67%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -3,7 +3,7 @@ public:
     bool checkIfExist(vector<int>& arr) {
         unordered_set<int> set;
         for(int i=0;i<arr.size();i++){
-            if(set.count(2*arr[i])>0 || ((arr[i]%2==0) && set.count(arr[i]/2)>0))
+            if(set.count(2*arr[i])>0 or ((arr[i]%2==0) and set.count(arr[i]/2)>0))
                 return true;
             set.insert(arr[i]);
         }
