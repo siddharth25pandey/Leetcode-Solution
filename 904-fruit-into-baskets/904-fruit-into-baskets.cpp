f480ee88bfdfa2e3f8f2db7819face53cce4d09c
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-04-07 11:43:18 +0530

    Time: 179 ms (59.75%), Space: 61.4 MB (49.96%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int s=fruits.size();
+        unordered_map<int,int>m;
+        int k=2;
+        int i=0,j=0;
+        int maxi=0;
+        while(j<s)
+        {
+            m[fruits[j]]++;
+            if(m.size()<=k)
+            {
+                maxi=max(maxi,(j-i+1));j++;
+            }
+            else if(m.size()>k)
+            {
+                while(m.size()>k)
+                {
+                      m[fruits[i]]--;
+                if(m[fruits[i]]==0)m.erase(fruits[i]);
+                i++;
+                }
+                
+              
+                j++;
+            }
+            
+        }
+        return maxi;
+    }
+};
