542bbe28109aac20bdfd7ff79691c25a920f0ef6
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-05-10 22:16:48 +0530

    Time: 6 ms (16.27%), Space: 6.6 MB (42.22%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+         vector<vector<int>> ansArr;
+
+    void combinationSumHelper(int k, int n, vector<int>& combination, int num){
+        int sum=accumulate(combination.begin(),combination.end(),0);
+        if(k == 0){
+            if(sum == n ){
+                ansArr.push_back(combination);
+            }
+            return;
+        }
+        
+        if(num > 9) return;
+        
+        combination.push_back(num);  // Including num in combination
+        combinationSumHelper(k - 1, n, combination, num + 1);
+        combination.pop_back(); // Not including num in combination
+        combinationSumHelper(k, n, combination, num + 1);    
+        
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        
+        vector<int> combination;
+        combinationSumHelper(k, n, combination, 1);
+        return ansArr;
+    }
+};
