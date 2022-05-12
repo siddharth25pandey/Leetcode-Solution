a070df6e85dbc0d9bcbb30e0fcb09f537f691444
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-05-12 10:09:50 +0530

    Time: 167 ms (13.34%), Space: 45.7 MB (5.78%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,15 +1,31 @@
 class Solution {
+    vector<vector<int>> ans;
+    void per(vector<int>& nums, int l, int r)
+    {
+    if(l==r) ans.push_back(nums);
+    else 
+    {
+    for(int i=l;i<=r;i++)
+    {
+        swap(nums[l],nums[i]);
+        per(nums,l+1,r);
+        swap(nums[l],nums[i]);
+    }
+    }
+    }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>>ans;
-        vector<int>p=nums;
-       while(1)
-        {   
-           next_permutation(nums.begin(),nums.end());
-           
-            ans.push_back(nums);
-           if(nums==p) break;
+       per(nums,0,nums.size()-1);
+        set<vector<int>>s;
+        for(auto i:ans)
+        {
+            s.insert(i);
         }
-        return ans;
+         vector<vector<int>>final_ans;
+        for(auto i:s)
+        {
+            final_ans.push_back(i);
+        }
+        return final_ans;
     }
 };
