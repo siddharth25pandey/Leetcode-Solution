6ff2d42921e764a2c1924f95a6f62ecc168ed2ae
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-05-12 13:48:55 +0530

    Time: 8 ms (59.70%), Space: 15.2 MB (14.42%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -10,36 +10,26 @@
 class Solution {
     
 public:
-   /* void helper(TreeNode* root, unordered_map<TreeNode*,TreeNode*>&m)
+   void helper(TreeNode* root, unordered_map<TreeNode*,TreeNode*>&m)
     {
         if(!root) return;
         if(root->left)
         {
             m.insert({root->left,root});
         }
-        else 
+       if(root->right)
         {
             m.insert({root->right,root});
         }
         helper(root->left,m);
         helper(root->right,m);
-    }*/
-    void dfs(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& m) {
-        if(!root) return;
-        if(root->left) {
-            m.insert({root->left, root});
-        }
-        if(root->right) {
-            m.insert({root->right, root});
-        }
-        dfs(root->left, m);
-        dfs(root->right, m);
     }
+   
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         vector<int>ans;
         queue<TreeNode*>q;
         unordered_map<TreeNode*,TreeNode*>m;
-        dfs(root,m);
+        helper(root,m);
         q.push(target);
         unordered_map<TreeNode*,bool>visit;
         while(!q.empty())
@@ -69,32 +59,7 @@ public:
             if(k<0) break;
         }
           return ans;
-         /*unordered_map<TreeNode*, TreeNode*> m; //{current, parent}
-        queue<TreeNode*> q;
-        dfs(root, m);
-        q.push(target);
-        vector<int> res;
-        unordered_map<TreeNode*, bool> visited;
-        while(!q.empty()) {
-            int len = q.size();
-            while(len--) {
-                TreeNode* curr = q.front(); q.pop();
-                if(!k) res.push_back(curr->val);
-                visited[curr] = true;
-                if(curr->left && !visited[curr->left]) {
-                    q.push(curr->left);
-                }
-                if(curr->right && !visited[curr->right]) {
-                    q.push(curr->right);
-                }
-                if(m.find(curr) != m.end() && visited[m[curr]] == false) {
-                    q.push(m[curr]);
-                }
-            }
-            k--;
-            if(k < 0) break;
-        }
-        return res;*/
+         
     }
   
 };
