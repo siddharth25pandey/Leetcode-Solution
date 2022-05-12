5de0d12eb1ccfeebbc08b457df3b9727b7930976
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-05-12 18:33:47 +0530

    Time: 18 ms (22.49%), Space: 9.6 MB (23.52%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    
+public:
+   bool dfs(vector<vector<int>>&graph,vector<bool>&visit, int current, int parent=-1)
+   {
+       if(visit[current]) return true;   // reached already visited node -  cycle detected
+       visit[current]=true;
+       for(auto i:graph[current])
+       {
+           if(i!=parent and dfs(graph,visit,i,current)) return true;
+       }
+       return false; // no cycle found
+      
+        
+   }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+       int n=edges.size();
+       vector<vector<int>>graph(n+1);
+       vector<bool>visit(n+1);
+       for(auto i:edges)
+       {    fill(begin(visit), end(visit), false); 
+           graph[i[0]].push_back(i[1]);
+            graph[i[1]].push_back(i[0]);
+           if(dfs(graph,visit,i[0]))return i;
+       }
+       return {};
+    }
+};
