58eb429a9393d31d2cb537d1301cb39087ce9cef
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-05-14 12:08:16 +0530

    Time: 7 ms (12.36%), Space: 6 MB (48.71%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
        int ans=0;  
+       /* string s=to_string(n);
+        for(int i=0;i<=s.size();i++)
+        {
+            if(s[i]=='1')ans++;
+        }*/
         while(n>0){  
 			ans++;
             n=n&(n-1); 
