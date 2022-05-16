1c42e12088dca1806a2e49d411ad8c1cb00d53c4
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-05-16 23:36:31 +0530

    Time: 94 ms (70.26%), Space: 11.7 MB (74.94%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int dp[10000];
-    int helper(int amt, vector<int>&v)
-    {   if(amt==0) return 0;
-        if(dp[amt]!=-1) return dp[amt];
-        int ans=INT_MAX;
-        for(auto i:v)
+   int dp[10000];
+int helper(vector<int>&nums, int amt) // Using Recursion
+{   
+    if(amt==0) return 0;
+    if(dp[amt]!=-1) return dp[amt];
+    int ans=INT_MAX;
+    for(auto i:nums)
+    {
+        if(amt-i>=0)
         {
-            if(amt-i>=0)
-            {
-                ans=min(ans+0LL,helper(amt-i,v)+1LL);
-            }
+            ans=min(ans+0LL,helper(nums,amt-i)+1LL);
         }
-        return dp[amt]=ans;
     }
-    int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        return helper(amount,coins)==INT_MAX?-1:helper(amount,coins);
+    return dp[amt]=ans;
+}
+    int coinChange(vector<int>& nums, int amt) {
+         memset(dp,-1,sizeof(dp));
+         int p=helper(nums,amt);
+    return p==INT_MAX?-1:p;
     }
 };
