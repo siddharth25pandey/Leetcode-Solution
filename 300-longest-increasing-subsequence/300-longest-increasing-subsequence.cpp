be84c2d72b26c53af2cce6a0af62dd8b117cc239
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-05-16 22:20:00 +0530

    Time: 401 ms (32.86%), Space: 10.4 MB (66.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,9 +1,9 @@
-
+vector<int>dp(2550,-1);
 class Solution {
   
 public:
     
-    int dp[2515];
+    
     int helper(int i, vector<int>&v)
     {   if(dp[i]!=-1) return dp[i];
         int ans=1;
@@ -17,7 +17,7 @@ public:
         return dp[i]=ans;
     }
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
+        fill(dp.begin(),dp.end(),-1);
         int ans=1;
         
         for(int i=0;i<nums.size();i++)
