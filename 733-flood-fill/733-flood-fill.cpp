755cfe7f281297d166274b6509181a3c679349af
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-05-20 19:57:06 +0530

    Time: 18 ms (24.40%), Space: 14 MB (81.34%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& mat, int sr, int sc, int upd) {
-        int m = mat.size();
-        int n = mat[0].size();
-        queue<pair<int, int>>q;
-        q.push({sr, sc});
-        int org = mat[sr][sc];
-        vector<vector<int>>vis(m , vector<int> (n, 0));
-        mat[sr][sc] = upd;
-        vis[sr][sc] = 1;
-        vector<int>dx = {1, 0 , -1 , 0};
-        vector<int>dy = {0 , 1, 0 , -1};
-        while(!q.empty()){
-            auto [x,y] = q.front();
-            q.pop();
-            
-            for(int i = 0;i<4;i++){
-                int newx = x + dx[i];
-                int newy = y + dy[i];
-                if(newx>=0 and newx <m and newy>=0 and newy<n and mat[newx][newy]==org and vis[newx][newy]==0){
-                    mat[newx][newy] = upd;
-                    q.push({newx, newy});
-                    vis[newx][newy] = 1;
-                }
-            }
-            
-            
-        }
-        
-        
-        return mat;
-    
+    void dfs(int i, int j,int orgc, int newc,vector<vector<int>>& image)
+    {
+        int n=image.size();
+        int m=image[0].size();
+        if(i<0 or j<0 or i>=n or j>=m or image[i][j]!=orgc)return;
+        image[i][j]=newc;
+        dfs(i+1,j,orgc,newc,image);
+        dfs(i-1,j,orgc,newc,image);
+        dfs(i,j+1,orgc,newc,image);
+        dfs(i,j-1,orgc,newc,image);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int orgc=image[sr][sc];
+        if(orgc!=newColor)
+        dfs(sr,sc,orgc,newColor,image);
+        return image;
     }
 };
