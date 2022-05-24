7721678fdb7644d106e49efd1fdc6ea2cf204eec
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-05-24 07:04:34 +0530

    Time: 3 ms (88.93%), Space: 7.3 MB (42.55%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int ans=0;
+        stack<int>st;
+        st.push(-1);
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')st.push(i);
+            else 
+            {
+                st.pop();
+                if(st.empty())st.push(i);
+                else ans=max(ans,i-st.top());
+            }
+            
+        }
+        return ans;
+    }
+};
