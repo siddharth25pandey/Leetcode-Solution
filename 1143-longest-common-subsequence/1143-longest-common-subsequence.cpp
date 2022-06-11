d5d570442525699fb54254a48b848ad917d0da57
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-06-11 21:23:47 +0530

    Time: 83 ms (5.09%), Space: 15.4 MB (10.58%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int dp[1005][1005];
+    int memo[1005][1005];
+     int dp[1005][1005];
     int LCS(string x, string y, int n,int m)
     {
         if(n==0 or m==0) dp[n][m]=0;
@@ -12,7 +13,7 @@ public:
         else return dp[n][m]=max(LCS(x,y,n-1,m),LCS(x,y,n,m-1));
        
     }
-     int longestCommonSubsequence(string &text1, string &text2,int m,int n,int memo[][1001]) {
+     int longestCommonSubsequence(string &text1, string &text2,int m,int n) {
         if(m==0 || n==0){
             return 0;
         }
@@ -20,9 +21,9 @@ public:
             return memo[m][n];
         }
         if(text1[m-1]==text2[n-1]){
-            return memo[m][n]= 1+longestCommonSubsequence(text1,text2,m-1,n-1,memo);
+            return memo[m][n]= 1+longestCommonSubsequence(text1,text2,m-1,n-1);
         }else{
-            return memo[m][n]= max(longestCommonSubsequence(text1,text2,m-1,n,memo),longestCommonSubsequence(text1,text2,m,n-1,memo));
+            return memo[m][n]= max(longestCommonSubsequence(text1,text2,m-1,n),longestCommonSubsequence(text1,text2,m,n-1));
         }
     }
     int longestCommonSubsequence(string text1, string text2) {
@@ -50,12 +51,12 @@ public:
         /*memset(dp, -1, sizeof(dp));
         return LCS(text1,text2,text1.size(),text2.size());*/
          int m=text1.size(),n=text2.size();
-        int memo[1001][1001];
+       
         for(int i=0;i<1001;i++){
             for(int j=0;j<1001;j++){
                 memo[i][j]=-1;
             }
         }
-        return longestCommonSubsequence(text1,text2,m,n,memo);
+        return longestCommonSubsequence(text1,text2,m,n);
     }
 };
