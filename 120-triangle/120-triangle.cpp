0fe0812b029be76d9fe92aecbc7caf626da7e578
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-06-13 08:56:58 +0530

    Time: 8 ms (61.32%), Space: 8.9 MB (41.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -10,7 +10,7 @@ public:
             {
                 if(i==0 and j==0)dp[i][j]=triangle[i][j];
                 else if(j==0) dp[i][j]= dp[i-1][j]+triangle[i][j];
-                  else if(j==dp[i].size()-1) dp[i][j]=dp[i-1][j-1]+triangle[i][j];
+                 else if(j==dp[i].size()-1) dp[i][j]=dp[i-1][j-1]+triangle[i][j];
                 else dp[i][j]=min( dp[i-1][j]+triangle[i][j], dp[i-1][j-1]+triangle[i][j]);
                   if(i==n-1) ans=min(ans,dp[n-1][j]);
             }
