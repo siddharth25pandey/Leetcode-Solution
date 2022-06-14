be94e2ed9ceb26594b3b4b6c7970929aff95151a
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-06-14 08:32:22 +0530

    Time: 78 ms (74.44%), Space: 48.5 MB (43.16%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int x=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(x<i)
+            {
+               return false;
+            }
+             x=max(x,nums[i]+i);
+        }
+        return true;
+    }
+};
