9be15bd531bebcbc2fb0e111654a0f69a7e0d928
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-06-19 12:10:26 +0530

    Time: 11 ms (46.40%), Space: 7.3 MB (11.22%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+     string normalize_pattern(string word) {
+        unordered_map<char, char> m;
+        char curr = 'a';
+        
+        for (auto i : word)
+            if (!m[i]) m[i] = curr++;
+        
+        for (int i = 0; i < word.size(); i++) word[i] = m[word[i]];
+        
+        return word;
+        
+    }
+public:
+    bool isIsomorphic(string s, string t) {
+       return normalize_pattern(s) == normalize_pattern(t);
+    }
+};
