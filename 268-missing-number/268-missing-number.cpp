19aebf4a474586bb0a230591bf068f4c04c8e8ba
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-06-20 15:45:05 +0530

    Time: 27 ms (58.32%), Space: 18.1 MB (18.87%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -3,14 +3,14 @@ public:
     int missingNumber(vector<int>& nums) {
       
          //return nums.size() * (nums.size() + 1) / 2 - accumulate(nums.begin(), nums.end(), 0);
-        int result = nums.size(); // initialize result to size of vector
-        int i=0; // initialize i to 0
-        for(int num:nums){ // for each number in vector
-            result ^= num; // XOR result with number
-            result ^= i; // XOR result with i
-            i++; // increment i
+        int result = nums.size(); 
+        int start=0;
+        for(int i:nums){ 
+            result ^= i;
+            result ^= start++;
         }
-        return result; // return result that is the missing number
+        return result; 
     }
     
 };
+    
