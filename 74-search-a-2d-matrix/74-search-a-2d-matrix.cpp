8420c5fd32706ed859c450179ab63465c25f02b4
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-06-22 14:42:46 +0530

    Time: 9 ms (28.93%), Space: 9.5 MB (78.67%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    bool binary_search_sid(vector<int>&v, int target)
+    {
+        int start=0,end=v.size()-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(v[mid]==target)return true;
+            if(v[mid]>target)end=mid-1;
+            if(v[mid]<target)start=mid+1;
+        }
+        return false;
+    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
        /*int row = 0, col = matrix[0].size()-1;
         
@@ -14,7 +26,8 @@ public:
         return false;*/
         for(auto &i:matrix)
         {
-            if(binary_search(i.begin(),i.end(),target))return true;
+           // if(binary_search(i.begin(),i.end(),target))return true;
+            if(binary_search_sid(i,target))return true;
         }
         return false;
     }
