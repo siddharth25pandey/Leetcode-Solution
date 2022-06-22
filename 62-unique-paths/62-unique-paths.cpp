41c30e9930b75901208d87860e98dcc7dd64bddf
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-06-22 22:47:49 +0530

    Time: 5 ms (17.59%), Space: 5.7 MB (98.34%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int dp[m][n];
+       /* int dp[m][n];
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -11,6 +11,13 @@ public:
                    dp[i][j]=dp[i-1][j]+dp[i][j-1]  ;
             }
         }
-        return dp[m-1][n-1];
+        return dp[m-1][n-1];*/
+        int N = n + m - 2;// how much steps we need to do
+            int k = m - 1; // number of steps that need to go down
+            double res = 1;
+            // here we calculate the total possible path number 
+            // Combination(N, k) = n! / (k!(n - k)!)
+            for (int i = 1; i <= k; i++)res = res * (N - k + i) / i;
+            return (int)res;
     }
 };
