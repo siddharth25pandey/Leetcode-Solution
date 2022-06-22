aa5d86a1c2a1a3c3e262951b2ae25ba5ed64da89
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-06-22 14:50:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.09%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+       // return pow(x,n);
+       double res = 1;
+        while (n) {
+            if (n % 2) res = n > 0 ? res * x : res / x;
+            x = x * x;
+            n /= 2;
+        }
+        return res;
+    }
+};
