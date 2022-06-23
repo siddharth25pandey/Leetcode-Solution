fc43b9442c45382e93ab9671bea94ca3216ac384
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-06-23 21:40:33 +0530

    Time: 813 ms (8.02%), Space: 49.5 MB (10.39%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+         long long int n = nums.size();  
+        sort(nums.begin() , nums.end()); 
+        vector<vector<int>> ans;  
+        set<vector<int>> store;   // to store and remove the duplicate answers
+		
+        for( long long int i = 0 ; i < n; i++){
+		
+            for( long long int j = i + 1; j < n ; j++){
+			
+                long long int new_target  =  (target - (nums[i]*1LL) - (nums[j]*1LL));
+                
+                long long int x = j+1 , y = n-1;
+                
+                while(x < y){
+				
+                   long long int sum = nums[x]*1LL + nums[y]*1LL;
+                    
+                    if(sum > new_target) y--;
+                    else if(sum < new_target ) x++;
+                    else  {
+                        store.insert({nums[i] , nums[j] , nums[x] , nums[y]});
+                        x++;
+                        y--;
+                    };
+                }
+            }
+        }
+		
+        for(auto i : store){
+            ans.push_back(i);  
+        }
+		
+        return ans;
+    }
+};
