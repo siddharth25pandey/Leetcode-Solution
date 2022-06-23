0d83bac11d859053ccfbdc25b9bac4451e674645
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-06-23 21:44:10 +0530

    Time: 333 ms (20.89%), Space: 49.3 MB (16.27%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-         long long int n = nums.size();  
         sort(nums.begin() , nums.end()); 
         vector<vector<int>> ans;  
-        set<vector<int>> store;   
+        set<vector<int>> s;   
 		
-        for(int i = 0 ; i < n; i++){
+        for(int i = 0 ; i < nums.size(); i++){
 		
-            for(int j = i + 1; j < n ; j++){
+            for(int j = i + 1; j < nums.size(); j++){
 			
                 long long int new_target  =  (target - (nums[i]*1LL) - (nums[j]*1LL));
                 
-                 int x = j+1 , y = n-1;
+                 int x = j+1 , y = nums.size()-1;
                 
                 while(x < y){
 				    long long int sum = nums[x]*1LL + nums[y]*1LL;                    
                     if(sum > new_target) y--;
                     else if(sum < new_target ) x++;
                     else  {
-                        store.insert({nums[i] , nums[j] , nums[x] , nums[y]});
+                        s.insert({nums[i] , nums[j] , nums[x] , nums[y]});
                         x++;
                         y--;
                     };
@@ -28,7 +27,7 @@ public:
             }
         }
 		
-        for(auto &i : store)ans.push_back(i); 		
+        for(auto &i : s)ans.push_back(i); 		
         return ans;
     }
 };
