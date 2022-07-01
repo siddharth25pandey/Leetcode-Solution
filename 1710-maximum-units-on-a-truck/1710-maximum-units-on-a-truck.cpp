c549ecab9c03b811da754c327a0a13c115ec4fd5
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-01 12:04:48 +0530

    Time: 51 ms (81.25%), Space: 15.9 MB (95.14%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -4,13 +4,14 @@ public:
         return p1[1]>p2[1];
     }
     int maximumUnits(vector<vector<int>>& b, int t) {
-        sort(b.begin(),b.end(),cmp);
+       // sort(b.begin(),b.end(),cmp);
+        sort(b.begin(), b.end(), [](auto& a, auto& b) { return b[1] < a[1];});
         int ans=0;
         for(auto &i: b){
-            int x=min(i[0],t);  //choose minimum boxes from available boxes and capacity left
-            ans+=(x*i[1]);  //adding units in ans
-            t-=x;  //reduce the capacity
-            if(!t) break;  //capacity full
+            int x=min(i[0],t);  
+            ans+=(x*i[1]); 
+            t-=x;  
+            if(!t) break;  
         }
         return ans;
     }
