3397f2dc3fe83b50a9db7538f39c773ac0d7267a
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-06 22:09:36 +0530

    Time: 73 ms (80.74%), Space: 45.1 MB (31.93%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -1,29 +1,54 @@
 class Trie {
 public:
+    struct TrieNode{
+    struct TrieNode *child[26];
+    bool isEnd;
+};
+TrieNode* node;
     vector<string>v;
     Trie() {
-        
+          node=new TrieNode();
     }
     
     void insert(string word) {
-        v.push_back(word);
+        //v.push_back(word);
+        TrieNode* cur=node;
+    for(char c: word){
+        if(cur->child[c-'a']==NULL) cur->child[c-'a']=new TrieNode();
+        cur=cur->child[c-'a'];
+    }
+    cur->isEnd=true;
     }
     
     bool search(string word) {
-        auto it= find(v.begin(),v.end(),word);
+        /*auto it= find(v.begin(),v.end(),word);
         if(it!=v.end()) return true;
-        return false;
+        return false;*/
+        TrieNode* cur=node;
+    for(char c: word){
+        if(cur->child[c-'a']==NULL)return false;
+        cur=cur->child[c-'a'];
+    }
+    if(cur->isEnd) return true;
+    return false;
     }
     
     bool startsWith(string prefix) {
-        for(int i=0;i<v.size();i++)
+        /*for(int i=0;i<v.size();i++)
         {
             string p=v[i].substr(0,prefix.size());
             if(p==prefix) return true;
         }
-        return false;
+        return false;*/
+         TrieNode* cur=node;
+    for(char c: prefix){
+        if(cur->child[c-'a']==NULL)return false;
+        cur=cur->child[c-'a'];
+    }
+    return true;
     }
 };
+
 
 /**
  * Your Trie object will be instantiated and called as such:
