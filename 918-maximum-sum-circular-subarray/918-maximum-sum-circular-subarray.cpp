559b6d0738001b966a787b30b9d12c2b31672159
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-08 19:19:24 +0530

    Time: 126 ms (20.82%), Space: 39.9 MB (30.13%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int sum=nums[0];
+        // Variation of Maximum Subarray Problem (Kadane's Algorithm)
         int max_sum=nums[0],min_sum=nums[0];
         int x=nums[0],y=nums[0];
         for(int i=1;i<nums.size();i++)
         {
-            sum+=nums[i];
+           
             x=max(nums[i],x+nums[i]);
             max_sum=max(max_sum,x);
              y=min(nums[i],y+nums[i]);
             min_sum=min(min_sum,y);
             
         }
-        if(sum==min_sum) return max_sum;
-        return max(max_sum,sum-min_sum);
+        if(accumulate(nums.begin(),nums.end(),0)==min_sum) return max_sum;
+        return max(max_sum,accumulate(nums.begin(),nums.end(),0)-min_sum);
     }
 };
