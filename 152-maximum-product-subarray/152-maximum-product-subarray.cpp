caa1afaea85fd13824f266e421745673a541e53d
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-08 19:26:05 +0530

    Time: 21 ms (9.75%), Space: 13.7 MB (71.99%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -3,12 +3,10 @@ public:
     int maxProduct(vector<int>& nums) {
         int max_pro=nums[0],x=nums[0],y=nums[0];
         for(int i=1;i<nums.size();i++)
-        {   if(nums[i]<0)
-        {
-            swap(x,y);
-        }
+        {   if(nums[i]<0)swap(x,y);
+       
             x=max(nums[i],nums[i]*x);
-            y=min(nums[i],nums[i]*y);
+           y=min(nums[i],nums[i]*y);
             max_pro=max(x,max_pro);
         }
         return max_pro;
