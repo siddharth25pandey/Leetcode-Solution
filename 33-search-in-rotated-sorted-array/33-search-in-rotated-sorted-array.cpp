164c81b0042893a06ab4cda73bcc303d227707ea
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-13 21:42:55 +0530

    Time: 3 ms (88.27%), Space: 11.1 MB (29.04%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-       /*int l = 0, r = nums.size()-1;
-        while (l <= r) {
-            int mid = (l+r) / 2;
-            if (target == nums[mid])
-                return mid;
-            // there exists rotation; the middle element is in the left part of the array
-            if (nums[mid] > nums[r]) {
-                if (target < nums[mid] && target >= nums[l])
-                    r = mid - 1;
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target) return mid;
+            
+            if(nums[mid]>=nums[low]){
+                //left half is sorted
+                if(target>=nums[low] && target<nums[mid])
+                    high = mid-1;
                 else
-                    l = mid + 1;
+                    low = mid+1;
             }
-            // there exists rotation; the middle element is in the right part of the array
-            else if (nums[mid] < nums[l]) {
-                if (target > nums[mid] && target <= nums[r])
-                    l = mid + 1;
+            else{
+                //right half is sorted
+                if(target>nums[mid] && target<=nums[high])
+                    low = mid+1;
                 else
-                    r = mid - 1;
+                    high = mid-1;
             }
-            // there is no rotation; just like normal binary search
-            else {
-                if (target < nums[mid])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
-        }
-        return -1;*/
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==target) return i;
         }
         return -1;
-    
     }
 };
