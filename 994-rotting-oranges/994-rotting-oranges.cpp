f28389faa4f9cdcd677e2c9f5bbea851249bb48e
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-13 23:53:14 +0530

    Time: 16 ms (30.15%), Space: 12.9 MB (98.37%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>&grid, int i, int j, int min)
+    {
+        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or  grid[i][j]==0 or (1 < grid[i][j] and grid[i][j] < min))return ;
+        grid[i][j]=min;
+        dfs(grid,i+1,j,min+1);
+         dfs(grid,i-1,j,min+1);
+         dfs(grid,i,j+1,min+1);
+         dfs(grid,i,j-1,min+1);
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]==2) dfs(grid,i,j,2);
+
+            }
+        }
+        int min=2;
+        for(auto &i:grid)
+        {
+            for(auto &j:i)
+            {
+                if(j==1) return -1;
+                min=max(min,j);
+            }
+        }
+        return min-2;
+    }
+};
