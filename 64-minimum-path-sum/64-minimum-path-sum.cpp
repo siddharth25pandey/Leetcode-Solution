5e0405e3add5facf91db8d50150cabef0972bfcd
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-14 19:51:07 +0530

    Time: 16 ms (46.22%), Space: 9.7 MB (90.08%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -2,26 +2,21 @@
 class Solution {
    
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-          int row=grid.size();
-    int column=grid[0].size();
-    int sum=0;
-    for(int i=0;i<column;i++) {	
-		sum+=grid[0][i];                                      //Filling the first row ,only movement in right is possible here
-        grid[0][i]=sum;
-        }
-    sum=0;
-    for(int i=0;i<row;i++) {                                            
-        sum+=grid[i][0];                                    //Filling the first column ,only movement in downward direction  
-        grid[i][0]=sum;
-        }
-    for(int i=1;i<row;i++)
-		{
-        for(int j=1;j<column;j++)
-		{                                                                            
-        grid[i][j]=min(grid[i-1][j],grid[i][j-1])+grid[i][j];//Filling the remaining cells using the minimum value and adding to the current cell value   
-        }
+    int f(vector<vector<int>>& grid, int row, int col)
+    {
+        if(row == 0 and col == 0) return grid[row][col]; //When we reached the first row, we could only move horizontally.
+        if(row == 0) return grid[row][col] + f(grid, row, col - 1);  // When we reached the first column, we could only move vertically
+        if(col == 0) return grid[row][col] + f(grid, row - 1, col);
+        return grid[row][col] + min(f(grid, row - 1, col), f(grid, row, col - 1));
+
     }
-    return grid[row-1][column-1];      
+    int minPathSum(vector<vector<int>>& grid) {
+        // return f(grid, grid.size()- 1, grid[0].size()- 1); // TLE
+        for(int i = 1; i < grid.size(); i++) grid[i][0] += grid[i - 1][0];
+        for(int j = 1; j < grid[0].size(); j++)grid[0][j] += grid[0][j - 1]; 
+        for(int i = 1; i < grid.size(); i++) {
+            for(int j = 1; j < grid[0].size(); j++) grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+        }
+        return grid[grid.size() - 1][grid[0].size() - 1];
     }
 };
