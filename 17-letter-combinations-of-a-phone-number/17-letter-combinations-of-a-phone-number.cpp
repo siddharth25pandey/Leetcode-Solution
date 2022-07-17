3a948587bd3dfcdfea53a0960effeeeaf68e908b
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-17 16:46:11 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (19.54%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -13,8 +13,7 @@ public:
             if(s.size() == digits.size()) ans.push_back(s);
             else{
                 string temp = phone[digits[s.size()] - '0'];
-                for(auto &i : temp)
-                    q.push(s + i);
+                for(auto &i : temp)q.push(s + i);    
             }
         }
         return ans;
