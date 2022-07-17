6d1f7d9b3d0fd97e1ab122435a54d4f9ae9441de
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-17 16:45:59 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (57.43%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,53 +1,19 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        /*if(digits=="") return {};
-        unordered_map<int,string>m;
-        m[2]="abc";
-        m[3]="def";
-        m[4]="ghi";
-        m[5]="jkl";
-        m[6]="mno";
-        m[7]="pqrs";
-        m[8]="tuv";
-        m[9]="wxyz";
-        vector<string>v;
-       
-        
-        for(int i=0;i<digits.size();i++)
-        {       
-            string p;
-            p=m[digits[i]-'0'];
-            v.push_back(p);
-        }
-       
-        vector<string> mappings{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans{""};
-	for(auto digit : digits){
-		vector<string> extendCombination;
-		for(auto& currentCombination : ans)
-			for(auto newChar : mappings[digit - '2'])
-				extendCombination.push_back(currentCombination + newChar);                            
-		ans = extendCombination; // same as ans = extendCombination, just avoids copying each value. You Can also use swap(ans,extendCombination)
-	}
-	return ans;*/
         vector<string> phone = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        if(digits.empty())
-            return {};
-        
+        if(digits.empty())  return {};
         queue<string> q;
         vector<string> ans;
-        
         q.push("");
-
         while(!q.empty()){
             string s = q.front();
             q.pop();
             
-            if(s.length() == digits.length())
-                ans.push_back(s);
+            if(s.size() == digits.size()) ans.push_back(s);
             else{
-                string temp = phone[digits[s.length()] - '0'];
-                for(auto i : temp)
+                string temp = phone[digits[s.size()] - '0'];
+                for(auto &i : temp)
                     q.push(s + i);
             }
         }
