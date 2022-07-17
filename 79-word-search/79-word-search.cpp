e16e24283f5a1a70d9a89fa80c76ac0e18a58798
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-17 14:59:02 +0530

    Time: 425 ms (65.21%), Space: 8 MB (73.84%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -2,18 +2,10 @@ class Solution {
 public:
      bool dfs(int index , int x , int y , vector<vector<char>> &board , string &word)
    {
-       if(index == word.size()) // word exists in matrix
-           return true;
-       
-       if(x < 0 or y < 0 or x >= board.size() or y >= board[0].size() or board[x][y] == '.') // boundary check + visited check
-           return false;
-       
-       if(board[x][y] != word[index])
-           return false;
-       
+       if(index == word.size()) return true;     // word exists in matrix   
+       if(x < 0 or y < 0 or x >= board.size() or y >= board[0].size() or board[x][y] == '.' or board[x][y] != word[index]) return false; // boundary check + visited check
        char temp = board[x][y];
        board[x][y] = '.'; // marking it visited
-       
        // Move in 4 directions[UP , DOWN , LEFT , RIGHT]
        if(dfs(index + 1 , x - 1 , y , board , word) or dfs(index + 1 , x + 1 , y , board , word) or dfs(index + 1 , x , y - 1 , board , word) or dfs(index + 1 , x , y + 1 , board , word))
            return true;
