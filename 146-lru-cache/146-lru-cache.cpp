b7838b02d7dd263c6ffbc64f801b6620bd4e945f
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-19 22:01:52 +0530

    Time: 775 ms (20.72%), Space: 164.1 MB (96.00%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,34 +1,26 @@
 class LRUCache {
-    list<pair<int,int>>v;
-    unordered_map<int,list<pair<int,int>>::iterator>m;
-    int size;
 public:
-    LRUCache(int capacity) {
-        size=capacity;
-    }
-    
-    int get(int key) {
-        if(m.find(key)==m.end()) return -1; 
-         v.splice(v.begin(),v,m[key]);
-        return m[key]->second;
-    }
-    
-    void put(int key, int value) {
-        if(m.find(key)!=m.end())
-        {
-              v.splice(v.begin(),v,m[key]);
-             m[key]->second=value;
-            return;
-        }
-        if(v.size()==size)
-        {
-            auto i=v.back().first;
-            v.pop_back();
-            m.erase(i);
-        }
-        v.push_front({key,value});
-        m[key]=v.begin();
-    }
+   unordered_map <int, int> m, cnt;
+queue <int> q;
+int n;
+LRUCache(int capacity) : n(capacity) {}
+int get(int key) {
+	if (cnt.find(key) == cnt.end()) 
+		return -1;
+	q.push(key);
+	cnt[key]++;
+	return m[key];
+}
+void put(int key, int value) {
+	q.push(key);
+	cnt[key]++;
+	m[key] = value;
+	while (cnt.size() > n) {
+		int cur = q.front(); q.pop();
+		if (cnt[cur]-- == 1) 
+			cnt.erase(cur);
+	}
+}
 };
 
 /**
