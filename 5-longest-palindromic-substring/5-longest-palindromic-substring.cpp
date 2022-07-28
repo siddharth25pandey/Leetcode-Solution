8cac2722127b8707f52dde4d55abc770cf9e2e74
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-29 00:37:55 +0530

    Time: 474 ms (30.08%), Space: 25.6 MB (39.78%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -3,38 +3,32 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        if (n == 0)
-            return "";
-
+        if (!n) return "";
         // dp[i][j] will be 'true' if the string from index i to j is a palindrome.
         bool dp[n][n];
-
         //Initialize with false
-
         memset(dp, 0, sizeof(dp));
 
         //Every Single character is palindrome
-        for (int i = 0; i < n; i++)
-            dp[i][i] = true;
-
+        for (int i = 0; i < n; i++) dp[i][i] = 1;
         string ans = "";
         ans += s[0];
 
-        for (int i = n - 1; i >= 0; i--)
+        for (int i = n - 1; i >= 0; i--) // Reverse traversal
         {
-            for (int j = i + 1; j < n; j++)
+            for (int j = i + 1; j < n; j++) // Forward traversal
             {
                 if (s[i] == s[j])
                 {
                     //If it is of two character OR if its susbtring is palindrome.
-                    if (j - i == 1 || dp[i + 1][j - 1])
+                    if (j - i == 1 or dp[i + 1][j - 1])
                     {
                         //Then it will also a palindrome substring
-                        dp[i][j] = true;
+                        dp[i][j] = 1;
 
                         //Check for Longest Palindrome substring
-                        if (ans.size() < j - i + 1)
-                            ans = s.substr(i, j - i + 1);
+                        if (ans.size() < j - i + 1)ans = s.substr(i, j - i + 1);
+                            
                     }
                 }
             }
