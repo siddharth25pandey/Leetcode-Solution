c3b864b70818432cc4612e745d41c7543cb37016
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-28 20:49:06 +0530

    Time: 7 ms (12.25%), Space: 7.8 MB (47.09%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,10 +4,7 @@ public:
        if(!nums.size()) return 0;
         vector<int>dp(nums.size()+1,0);
         dp[1]=nums[0];
-        for(int i=2;i<=nums.size();i++)
-        {
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
-        }
+        for(int i=2;i<=nums.size();i++)dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
         return dp[nums.size()];
     }
 };
