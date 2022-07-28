561b9b177c0fa553275a5524c793a8cfb660adef
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-29 00:29:20 +0530

    Time: 140 ms (48.15%), Space: 14.6 MB (42.39%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int n) {
-         int dp[++n];
+         vector<int>dp(n+1,INT_MAX);
         dp[0] = 0;
         // more convenient to have the coins sorted
         sort(coins.begin(), coins.end());
         // populating our dp array
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <=n; i++) {
             // setting dp[0] base value to 1, 0 for all the rest
-            dp[i] = INT_MAX;
             for (auto &c: coins) {
                 if (i - c < 0) break;
                 // if it was a previously not reached cell, we do not add use it
                 if (dp[i - c] != INT_MAX) dp[i] = min(dp[i], 1 + dp[i - c]);
             }
         }
-        return dp[--n] == INT_MAX ? -1 : dp[n];
+        return dp[n] == INT_MAX ? -1 : dp[n];
     }
 };
