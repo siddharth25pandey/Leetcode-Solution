c6e9213a23d1e6fc0601afd55b16afd2ebcaba8c
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-07-28 11:32:02 +0530

    Time: 6 ms (90.64%), Space: 7.4 MB (7.79%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       unordered_map<char,int>m1,m2;
+        for(auto &i:s)m1[i]++;
+        for(auto &i:t)m2[i]++;
+        return m1==m2;
+    }
+};
