13df65fe80cb6100941edd31bab15cb4e7e5c39a
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-08-04 11:22:42 +0530

    Time: 3 ms (36.36%), Space: 5.8 MB (62.81%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int mirrorReflection(int p, int q) {
-         while (p % 2 == 0 && q % 2 == 0){ 
+         while (!(p % 2) and !(q % 2)){ 
             p/=2;
             q/=2;
         }
