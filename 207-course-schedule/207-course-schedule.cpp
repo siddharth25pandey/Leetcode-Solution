17d4151ca6ab31d95e5dc78d0e414dfb76445bab
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-08-11 22:23:42 +0530

    Time: 36 ms (49.84%), Space: 12.9 MB (97.75%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
-    bool iscycle(vector<int> adj[],vector<int> &vis,int id){
-        if(vis[id]==1)return true;  
-        if(!vis[id]){
-            vis[id]=1;
-            for(auto &i : adj[id]){
-                if(iscycle(adj,vis,i)) return true;
+    bool canFinish(int course, vector<vector<int>>& pre) {
+        vector<int>graph[course+1];
+        vector<int>indegree(course,0);
+        for(auto &i:pre)
+        {
+            graph[i[1]].push_back(i[0]);
+            indegree[i[0]]+=1;
+        }
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++)
+        {
+            if(!indegree[i])q.push(i);
+        }
+        vector<int>ans;
+        while(!q.empty())
+        {   
+        
+            int size=q.size();
+            while(size--)
+            {
+                int val=q.front();
+                q.pop();
+                ans.push_back(val);
+                for(auto &i:graph[val])
+                {
+                    indegree[i]-=1;
+                    if(!indegree[i])q.push(i);
+                }
             }
+            
+
+            
         }
-        vis[id] = 2;
-        return false;
-    }
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<int> adj[n],vis(n,0);
-        for(auto &i : pre)adj[i[1]].push_back(i[0]); // Undirected
-        for(int i=0;i<n;i++){
-            if(iscycle(adj,vis,i))return false;     
-        }
-        return true;
+
+        return ans.size()==course;
+        
+        
+        
     }
 };
