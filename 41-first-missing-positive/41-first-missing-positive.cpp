8d6c71b9b0ef8ae1116e5ee752c596014b289802
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-08-15 19:27:17 +0530

    Time: 239 ms (64.65%), Space: 82.9 MB (78.07%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        set<int>v;
+        /*set<int>v;
         for(auto &i:nums)
         {
             if(i>0)v.insert(i);
@@ -12,6 +12,14 @@ public:
          if(k!=i)return k;
             else k++;
         }
-        return k;
+        return k;*/
+         for(int i = 0; i < nums.size();) {
+            if(nums[i] >= 0 and nums[i] - 1 >= 0 and nums[i] - 1 < nums.size()) swap(nums[nums[i]-1], nums[i]);
+            if(nums[i] <= 0 or nums[i] == i+1 or nums[i] > nums.size() or  nums[nums[i]-1] == nums[i])i++;     
+         }
+        for(int i = 0; i < nums.size(); i++) if(nums[i] != i + 1) return i + 1;
+           
+               
+        return nums.size() + 1;
     }
 };
