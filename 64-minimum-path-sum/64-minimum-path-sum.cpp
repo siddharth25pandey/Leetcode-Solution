32ceda07eef4db9f2af22955059e560ce3a8186f
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-08-16 19:46:10 +0530

    Time: 17 ms (43.26%), Space: 9.7 MB (80.90%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -2,14 +2,14 @@
 class Solution {
    
 public:
-    int f(vector<vector<int>>& grid, int row, int col)
+   /* int f(vector<vector<int>>& grid, int row, int col)
     {
         if(row == 0 and col == 0) return grid[row][col]; //When we reached the first row, we could only move horizontally.
         if(row == 0) return grid[row][col] + f(grid, row, col - 1);  // When we reached the first column, we could only move vertically
         if(col == 0) return grid[row][col] + f(grid, row - 1, col);
         return grid[row][col] + min(f(grid, row - 1, col), f(grid, row, col - 1));
 
-    }
+    }*/
     int minPathSum(vector<vector<int>>& grid) {
         // return f(grid, grid.size()- 1, grid[0].size()- 1); // TLE
         for(int i = 1; i < grid.size(); i++) grid[i][0] += grid[i - 1][0];
