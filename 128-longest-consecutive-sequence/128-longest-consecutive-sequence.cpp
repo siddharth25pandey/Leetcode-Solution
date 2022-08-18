ada10a973c4a0516ae28d2e8c8e7a28a35610c7f
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-08-18 19:44:29 +0530

    Time: 190 ms (68.99%), Space: 45.6 MB (73.07%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-       /* if(!nums.size())return 0;
-        set<int>s(nums.begin(),nums.end());
-        vector<int>v(s.begin(),s.end());
-        int ans=1;
-        for(int i=0;i<v.size()-1;i++)
-        {
-            if(abs(v[i+1]-v[i])<=1)ans++;
-        }
-        return ans;*/
-        unordered_set<int>s(nums.begin(),nums.end());
-        int ans=0;
-        if(!nums.size())return 0;
-        
-        for(auto i:nums)
-        {
-            if(s.find(i-1)==s.end())
-            {
-                int curr_num=i;
-                int c=1;
-                  while(s.find(curr_num+1)!=s.end())
-            {
-                curr_num++;
-                c++;
-            }
-                 ans=max(ans,c);
-            }
-          
-           
-        }
-        return ans;
+      unordered_set<int> s(begin(nums), end(nums)); 
+	int ans = 0;
+	for(auto& num : s) {
+		int cur_longest = 1;
+		// find consecutive elements in the backward and forward direction from num
+		for(int j = 1; s.count(num - j); j++) s.erase(num - j), cur_longest++;
+		for(int j = 1; s.count(num + j); j++) s.erase(num + j), cur_longest++;
+		ans = max(ans, cur_longest);  // update longest to hold longest consecutive sequence till now
+	}
+	return ans;
     }
 };
