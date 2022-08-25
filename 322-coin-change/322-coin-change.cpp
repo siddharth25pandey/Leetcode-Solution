0d1090a489ab390b074e7f3db8fc4e32e037bf8a
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-08-25 16:55:33 +0530

    Time: 63 ms (95.71%), Space: 13.6 MB (74.72%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
+   /* int coinChange(vector<int>& coins, int amount) {
         sort(coins.begin(),coins.end());
         vector<int>dp(amount+1,INT_MAX);
         dp[0]=0;
@@ -15,5 +15,23 @@ public:
         }
         if(dp[amount]==INT_MAX)return -1;
         return dp[amount];
+    }*/
+   int dp[100001];
+    int helper(int amt, vector<int>&v)
+    {   if(amt==0) return 0;
+        if(dp[amt]!=-1) return dp[amt];
+        int ans=INT_MAX;
+        for(auto i:v)
+        {
+            if(amt-i>=0)
+            {
+                ans=min(ans+0LL,helper(amt-i,v)+1LL);
+            }
+        }
+        return dp[amt]=ans;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        memset(dp,-1,sizeof(dp));
+        return helper(amount,coins)==INT_MAX?-1:helper(amount,coins);
     }
 };
