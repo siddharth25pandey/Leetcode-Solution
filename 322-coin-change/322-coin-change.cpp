950b0f30e0037aa9e159c25e15c023d4ec1c8a15
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-08-25 16:56:13 +0530

    Time: 101 ms (79.75%), Space: 13.5 MB (74.72%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-   /* int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(),coins.end());
-        vector<int>dp(amount+1,INT_MAX);
-        dp[0]=0;
-        for(int i=1;i<=amount;i++)
-        {
-            for(auto &k:coins)
-            {
-                if(i-k<0)break;
-                if(dp[i-k]!=INT_MAX)dp[i]=min(dp[i],1+dp[i-k]);
-            }
-            
-        }
-        if(dp[amount]==INT_MAX)return -1;
-        return dp[amount];
-    }*/
-   int dp[100001];
+     int dp[100001];
     int helper(int amt, vector<int>&v)
     {   if(amt==0) return 0;
         if(dp[amt]!=-1) return dp[amt];
@@ -30,8 +14,24 @@ public:
         }
         return dp[amt]=ans;
     }
-    int coinChange(vector<int>& coins, int amount) {
+   int coinChange(vector<int>& coins, int amount) {
         memset(dp,-1,sizeof(dp));
         return helper(amount,coins)==INT_MAX?-1:helper(amount,coins);
+        /*sort(coins.begin(),coins.end());
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++)
+        {
+            for(auto &k:coins)
+            {
+                if(i-k<0)break;
+                if(dp[i-k]!=INT_MAX)dp[i]=min(dp[i],1+dp[i-k]);
+            }
+            
+        }
+        if(dp[amount]==INT_MAX)return -1;
+        return dp[amount];*/
     }
+  
+    
 };
