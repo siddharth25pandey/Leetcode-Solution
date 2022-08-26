daa427b0327a60889872c41af2d8e129557cf7b3
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-08-26 11:45:14 +0530

    Time: 15 ms (8.36%), Space: 12.2 MB (36.78%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,7 +1,35 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        /*int x=-1;
+       // next_permutation(nums.begin(),nums.end());
+        /*int break_index=-1;
+        for(int i=nums.size()-1;i>0;i--)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                break_index=i-1;
+                break;
+            }
+        }
+        if(break_index<0)
+        {
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        else 
+        {
+            for(int i=nums.size();i>=0;i--)
+            {
+                if(nums[i]>nums[break_index])
+                
+                {
+                    swap(nums[i],nums[break_index]);
+                    reverse(nums.begin()+break_index+1,nums.end());
+                    break;
+                }
+            }
+        }*/
+        int x=-1;
         for(int i=nums.size()-1;i>0;i--)
         {
             if(nums[i]>nums[i-1])
@@ -23,7 +51,6 @@ public:
                 reverse(nums.begin()+x+1,nums.end());
                 break;
             }
-        }*/
-        next_permutation(nums.begin(),nums.end());
+        }
     }
 };
