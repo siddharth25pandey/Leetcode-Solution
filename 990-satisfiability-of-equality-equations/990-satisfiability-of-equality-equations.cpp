b161d775418e1506574e4a03134f01f508fd28e7
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-08-29 18:39:30 +0530

    Time: 3 ms (96.18%), Space: 11.2 MB (98.55%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int parent[1001];
-   
-    int find(int x){
-        if(parent[x]!=x)parent[x]= find(parent[x]);      
+    int find(int x, int parent[]){
+        if(parent[x]!=x)parent[x]= find(parent[x],parent);      
         return parent[x];
     }
-    void merge(int a,int b){
-        parent[find(a)] = find(b);
+    void merge(int a,int b,int parent[]){
+        parent[find(a,parent)] = find(b,parent);
     }
     bool equationsPossible(vector<string>& eq) {
-        for(int i=0;i<26;i++)
-        {
-            parent[i]=i;
-        }
+        int parent[1001];
+        for(int i=0;i<26;i++)  parent[i]=i;
+        for(auto &i:eq) if(i[1]=='=')merge(i[0]-'a',i[3]-'a',parent);
         for(auto &i:eq)
         {
-            if(i[1]=='=')
-            {
-                merge(i[0]-'a',i[3]-'a');
-            }
-        }
-        for(auto &i:eq)
-        {
-            if(i[1]=='!' and find(i[0]-'a')==find(i[3]-'a'))return false;  
+            if(i[1]=='!' and find(i[0]-'a',parent)==find(i[3]-'a',parent))return false;  
         }
         return true;
     }
