05664cbc672cc353e9faed771377e72a5ffa4063
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-08-30 21:39:53 +0530

    Time: 4 ms (85.62%), Space: 9.6 MB (68.84%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -14,8 +14,21 @@ public:
            
         }
         return i-1;*/
-        for (auto& i : arr) if (i <= k) k++;
-        return k;
+       /* for (auto& i : arr) if (i <= k) k++;
+        return k;*/
+        
+        if(k < arr[0])return k;
+            
+        int low=0, high=arr.size();
+        while(low < high)
+        {
+            int mid = low + ((high - low) >> 1);
+            if(arr[mid] - mid - 1 < k)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return low + k;
 
     }
 };
