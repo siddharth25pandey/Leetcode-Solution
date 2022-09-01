df5e9dfc006b9736b5586822d15798e37fbe3631
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-09-01 14:40:15 +0530

    Time: 121 ms (5.07%), Space: 445.4 MB (5.33%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
-    int longestValidParentheses(string s) {
-        int ans=0;
-        stack<int>st;
-        st.push(-1);
-        for(int i=0;i<s.size();i++)
+    int solve(string &s)
+{
+    int ans = 0, len = 0, sum = 0;
+    for (auto &i : s)
+    {
+        if (i == '(')
+            sum += 1;
+        else
+            sum -= 1;
+        if (sum < 0)
         {
-            if(s[i]=='(')st.push(i);
-            else 
-            {
-                st.pop();
-                if(st.empty())st.push(i);
-                else ans=max(ans,i-st.top());
-            }
-            
+            sum = 0;
+            len = 0;
         }
-        return ans;
+        else
+        {
+            len += 1;
+            if (sum == 0)
+                ans = max(ans, len);
+        }
+    }
+    return ans;
+}
+
+    int longestValidParentheses(string s) {
+        int normal, rev;
+    normal = solve(s); // O(n)
+    string s1="";
+    for (auto &i : s) // O(n)
+    {
+        s1 = (i == ')' ? '(' : ')') + s1;
+    }
+    rev = solve(s1); // O(n)
+    return max(rev, normal);
     }
 };
