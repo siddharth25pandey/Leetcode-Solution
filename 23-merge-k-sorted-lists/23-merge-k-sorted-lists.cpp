72e6292b9a5e72b8628cec2ee926eecf20b8e31b
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-09-17 19:12:25 +0530

    Time: 351 ms (14.80%), Space: 13.3 MB (76.17%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -9,24 +9,38 @@
  * };
  */
 class Solution {
-    public:
-     void insertattail(ListNode* &head, int val){
-        ListNode* n = new ListNode(val);
-        if (head==NULL){
-            head = n;
-            return;
-        }
-        ListNode* temp = head;
-        while(temp->next!=NULL){
-            temp = temp ->next;
-        }
-        temp->next = n;
-        n->next = NULL;
 
-    }
 public:
+    ListNode* merge_two_ll(ListNode* l1, ListNode*l2)
+    {
+        if(!l1)return l2;
+        if(!l2)return l1;
+        ListNode* head= NULL;
+        if(l1->val<=l2->val)
+        {
+            head=l1;
+            head->next=merge_two_ll(l1->next,l2);
+        }
+        else 
+        {
+            head=l2;
+            head->next=merge_two_ll(l1,l2->next);
+        }
+        return head;
+       
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0) return NULL;
+        if(!lists.size())return NULL;
+        ListNode* ans= lists[0];
+        for(int i=1;i<lists.size();i++)
+        {
+            ans=merge_two_ll(ans,lists[i]);
+        }
+        return ans;
+        
+        
+        // nlogn solution 
+       /* if(lists.size()==0) return NULL;
         vector<int>v;
         for(int i=0;i<lists.size();i++)
         {
@@ -52,7 +66,7 @@ public:
                 ans=ans->next;
             }
         }
-        return res;
+        return res;*/ 
         
     }
 };
