dbdbe36a19802efc57e06406555aa5878f43c71b
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2022-09-17 20:05:07 +0530

    Time: 27 ms (77.92%), Space: 13.1 MB (92.59%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,13 +1,35 @@
 class Solution {
 public:
-    bool canFinish(int course, vector<vector<int>>& pre) {
+    // Using Union-Find(DFU)
+    int find(int i, int par[])
+    {
+        if(par[i]!=i)par[i]=find(par[i],par);
+        return par[i];
+    }
+    void merge(int a, int b, int par[])
+    {
+        par[find(a,par)]=find(b,par);
+    }
+    bool canFinish(int course, vector<vector<int>>& pre) { // Topological sort
         vector<int>graph[course+1];
+        int par[10001];
+        for(int i=0;i<26;i++)
+        {
+            par[i]=i;
+        }
         vector<int>indegree(course,0);
         for(auto &i:pre)
         {
             graph[i[1]].push_back(i[0]);
             indegree[i[0]]+=1;
         }
+        /*for(auto &i:pre)
+        {
+            if(find(i[1],par)==find(i[0],par))return false;
+            merge(i[0],i[1],par);
+        }
+        return true;*/
+        
         queue<int>q;
         for(int i=0;i<indegree.size();i++)
         {
